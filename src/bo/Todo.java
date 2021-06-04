db0f2ac71ace1e67e897ba44b90a080e09edd806
chatelardfrancois <f.chatelard@hotmail.fr>
2021-06-04 09:39:44 +0200

    Ajout bdd

--- src/bo/Todo.java
+++ src/bo/Todo.java
@@ -1,5 +1,11 @@
 package bo;
 
+import java.time.LocalDate;
+
 public class Todo {
-    
+    private LocalDate date;
+    private int id;
+    private String texte;
+
+
 }
--- src/ihm/Main.java
+++ src/ihm/Main.java
@@ -1,7 +1,16 @@
 package ihm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-
+        String url = "jdbc:sqlite:todo_db.sqlite";
+        try{
+            Connection connection = DriverManager.getConnection(url);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
