16874d49bda9ea96ce6b31428ec8e4e152b367f8
chatelardfrancois <f.chatelard@hotmail.fr>
2021-06-04 09:46:41 +0200

    change propriete to perperties

--- src/dal/jdbc/Settings.java
+++ src/dal/jdbc/Settings.java
@@ -3,17 +3,17 @@ package dal.jdbc;
 import java.util.Properties;
 
 public class Settings {
-    private static Properties propriete;
+    private static Properties properties;
     static{
         try{
-            propriete = new Properties();
-            propriete.load(Settings.class.getResourceAsStream("settings.properties"));
+            properties = new Properties();
+            properties.load(Settings.class.getResourceAsStream("settings.properties"));
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     }
 
     public static String getProperty(String cle){
-        return propriete.getProperty(cle, null);
+        return properties.getProperty(cle, null);
     }
 }
