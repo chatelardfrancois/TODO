737b3a6b34c2fd6aa9ba7f5f76c5c694cb7f341e
chatelardfrancois <f.chatelard@hotmail.fr>
2021-06-04 09:43:58 +0200

    Ajouts classe Settings

--- src/dal/jdbc/Settings.java
+++ src/dal/jdbc/Settings.java
@@ -1,4 +1,19 @@
 package dal.jdbc;
 
+import java.util.Properties;
+
 public class Settings {
+    private static Properties propriete;
+    static{
+        try{
+            propriete = new Properties();
+            propriete.load(Settings.class.getResourceAsStream("settings.properties"));
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String getProperty(String cle){
+        return propriete.getProperty(cle, null);
+    }
 }
