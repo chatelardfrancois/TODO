f1f6174ad74204944347fbea8e5b105e4be3ef2b
chatelardfrancois <f.chatelard@hotmail.fr>
2021-06-04 09:41:16 +0200

    Constructeurs Todo

--- src/bo/Todo.java
+++ src/bo/Todo.java
@@ -7,5 +7,18 @@ public class Todo {
     private int id;
     private String texte;
 
+    public Todo(LocalDate date, String texte) {
+        this.date = date;
+        this.texte = texte;
+    }
 
+    public Todo(String texte) {
+        this.texte = texte;
+    }
+
+    public Todo(int id, LocalDate date, String texte) {
+        this.date = date;
+        this.id = id;
+        this.texte = texte;
+    }
 }
